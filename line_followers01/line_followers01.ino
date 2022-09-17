df5af92366ca81a47f068ea1a84aa5a64e8a1628
Vedant <rokadvedant03@gmail.com>
2022-09-17 15:28:55 +0530

    add commit

--- line_followers01/line_followers01.ino
+++ line_followers01/line_followers01.ino
@@ -1,11 +1,33 @@
-#define m11 = 1;
+//moter left -> motor 1 
+//motor right -> motor 2
+int motor_left_1 = 2 ;
+int motor_left_2  = 4 ;
+int motor_right_1 = 3 ;
+int motor_right_2 = 5 ;
+
+//int sen_5 = A5;
+//int sen_4 = A1;
+//int sen_3 = A2;
+//int sen_2 = A3;
+//int sen_1 = A4;
+
 
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(A1,INPUT);
+pinMode(A2,INPUT);
+pinMode(A3,INPUT);
+pinMode(A4,INPUT);
+pinMode(A5,INPUT);
 }
 
 void loop() {
+  
+  
   // put your main code here, to run repeatedly:
 
 }
