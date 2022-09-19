50a99a6370aa0ee32cea40fbbafd4552c6d93c2d
Vedant <rokadvedant03@gmail.com>
2022-09-19 21:04:32 +0530

    commit

--- line_followers01/line_followers01.ino
+++ line_followers01/line_followers01.ino
@@ -1,9 +1,9 @@
 //moter left -> motor 1 
 //motor right -> motor 2
-int motor_left_1 = 2 ;
-int motor_left_2  = 4 ;
-int motor_right_1 = 3 ;
-int motor_right_2 = 5 ;
+int motor_left_1 = 5 ;
+int motor_left_2  = 6 ;
+int motor_right_1 = 10 ;
+int motor_right_2 = 11 ;
 
 //int sen_5 = A5;
 //int sen_4 = A1;
@@ -11,22 +11,36 @@ int motor_right_2 = 5 ;
 //int sen_2 = A3;
 //int sen_1 = A4;
 
+int enable1 = 9;
+int enable2 = 8 ;
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
+  // put your setup code here, to run once  :
+pinMode(motor_left_1,OUTPUT);
+pinMode(motor_left_2,OUTPUT);
+pinMode(motor_right_1,OUTPUT);
+pinMode(motor_right_2,OUTPUT);
 pinMode(A1,INPUT);
 pinMode(A2,INPUT);
 pinMode(A3,INPUT);
 pinMode(A4,INPUT);
 pinMode(A5,INPUT);
+pinMode(enable1,OUTPUT);
+pinMode(enable2,OUTPUT);
+
+//ENABLE L293D
+digitalWrite(enable1,HIGH);
+digitalWrite(enable2,HIGH);
 }
 
 void loop() {
-  
+  int base = 255;
+  int lbias = 0;
+  int rbias = 0;
+  digitalWrite(motor_left_2,LOW);
+  analogWrite(motor_left_1,base + rbias);
+  digitalWrite(motor_right_2,LOW);
+  analogWrite(motor_right_1,base + lbias);
   
   // put your main code here, to run repeatedly:
 
